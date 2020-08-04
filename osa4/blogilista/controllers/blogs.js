173d10267935bdab2434840e56149a5452f96f5e
mmyotane <miika.myotanen@hotmail.com>
2020-08-04 19:52:19 +0300

    4.13

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -20,4 +20,23 @@ blogRouter.post("/", async (request, response) => {
   response.json(savedBlog.toJSON());
 });
 
+blogRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
+
+blogRouter.put("/:id", async (request, response, next) => {
+  const body = request.body;
+
+  const blog = {
+    content: body.content,
+    author: body.author,
+    url: body.url,
+    likes: 55,
+  };
+
+  await Blog.findByIdAndUpdate(request.params.id, blog, { new: true });
+  response.json(blog.toJSON());
+});
+
 module.exports = blogRouter;
