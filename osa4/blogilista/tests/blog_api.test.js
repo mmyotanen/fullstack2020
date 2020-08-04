4182d0958197d27a469e50a1b80a19cc49579c1d
mmyotane <miika.myotanen@hotmail.com>
2020-08-04 19:11:20 +0300

    4.10

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -23,6 +23,25 @@ test("id is not _id", async () => {
   expect(onkoId.id).toBeDefined();
 });
 
+test("new note can be added using post", async () => {
+  const newBlog = {
+    _id: "5a422a851b54a676234d17f7",
+    title: "post",
+    author: "Michael Chan",
+    url: "toimii",
+    likes: 7,
+    __v: 0,
+  };
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+
+  const response = await api.get("/api/blogs");
+  expect(response.body.length).toBe(helper.initialBlogs.length + 1);
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
