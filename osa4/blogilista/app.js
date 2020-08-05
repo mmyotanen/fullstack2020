6e9addeba4ab06a3a1b62dd4831fe74f37f285d8
mmyotane <miika.myotanen@hotmail.com>
2020-08-05 15:55:54 +0300

    4.19

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -2,6 +2,7 @@ const config = require("./utils/config");
 const express = require("express");
 require("express-async-errors");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -32,9 +33,10 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
+//app.use(middleware.tokenExtractor);
 app.use("/api/users", usersRouter);
 app.use("/api/blogs", blogRouter);
-
+app.use("/api/login", loginRouter);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
