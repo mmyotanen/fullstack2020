6b97d81a6b61d2dc22beedea2e50904439ba58da
mmyotane <miika.myotanen@hotmail.com>
2020-09-29 16:01:10 +0300

    5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,36 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
 
-export default Blog
+  const [allInfo, toggleAllInfo] = useState(false);
+
+  if (allInfo === true) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} - {blog.author}{" "}
+        <button onClick={() => toggleAllInfo(!allInfo)}> hide </button>
+        <br />
+        {blog.url} <br />
+        likes: {blog.likes} <button> like </button>
+        <br />
+        {blog.user.username}
+        <br />
+      </div>
+    );
+  } else {
+    return (
+      <div style={blogStyle}>
+        {blog.title} - {blog.author}{" "}
+        <button onClick={() => toggleAllInfo(!allInfo)}> view </button>
+      </div>
+    );
+  }
+};
+
+export default Blog;
